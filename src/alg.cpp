261f254c721306f12e0e35bac2e88803caeac22c
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2021-04-13 14:17:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ int countPairs1(int *arr, int len, int value) {
                 result++;
         }
     }
-  return result;
+   return result;
 }
 
 int countPairs2(int *arr, int len, int value) {
